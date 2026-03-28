f9441330fb5dc11905eab48107111d9719682e00
Saeed A <191458861+SaeedAAli@users.noreply.github.com>
2026-03-28 16:24:43 +0000

    Add variables for region, instance type, and AMI ID

--- inputs.tf
+++ inputs.tf
@@ -1,1 +1,18 @@
+variable "region" {
+  type = string
+  default = "eu-central-1"
+  description = "AWS Region"
+}
 
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+  description = "size of the instance"
+}
+
+variable "ami_id" {
+  type = string
+  description = "Amazon Instance ID for the EC2 instance"
+  
+}
